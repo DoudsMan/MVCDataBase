9f867279c57fb5650bc1bc28c382dd7cf774e6d6
Jacob Doudna <jdoudna90@gmail.com>
2020-03-27 21:16:18 -0500

    So many changes...

--- databaseAPI/Startup.cs
+++ databaseAPI/Startup.cs
@@ -11,7 +11,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.EntityFrameworkCore;
-
+using databaseAPI.Model;
 
 namespace databaseAPI
 {
@@ -29,7 +29,7 @@ namespace databaseAPI
         {
             services.AddControllers();
             var connection = Configuration.GetConnectionString("DBcsc484Context");
-            //services.AddDbContext<DBcsc484Context>(options => options.UseSqlServer(connection));
+            services.AddDbContext<DBcsc484Context>(options => options.UseSqlServer(connection));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
